d17141ea21bdb5a0061a2606f0853a185415d931
Rivael <135939684+RivaelManurung@users.noreply.github.com>
2025-08-24 15:57:46 +0700

    add code first

---
+++ backend/src/model/user.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    set(value) {
+      this.setDataValue("username", value.trim());
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue("password", value.trim());
+    },
+  }
+},{
+    tableName: "users",
+});
+
+module.exports = {
+  User,
+};
---
+++ backend/src/services/auth_service.js
@@ -0,0 +1,36 @@
+exports.registerUser = async (password, username) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      username: username,
+      password: hashedPassword,
+    });
+
+    user.password = undefined;
+    return user;
+  } catch (error) {
+    throw new Error("Error registering user");
+  }
+};
+
+exports.loginUser = async (username, password) => {
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      throw new Error("User not exist");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new Error("Password is incorrect");
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return { user, token };
+  } catch (error) {
+    throw new Error("Error logging in User");
+  }
+};
